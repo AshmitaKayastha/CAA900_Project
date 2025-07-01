6ba1535f1e70a2c9be18eee40f0a320a77ce62e9
Ashmita Kayastha <ashmee567@gmail.com>
2025-07-01 16:29:38 -0400

    Admin Dashboard and Courses Added

--- client/src/admin/showCourseAdmin.js
+++ client/src/admin/showCourseAdmin.js
@@ -1,101 +1,54 @@
 import React, { Component } from "react";
-// import { Link } from 'react-router-dom';
 import axios from "axios";
 import NavBar from "../components/NavBar";
 import { ToastContainer, toast } from "react-toastify";
 
-export default class UserList extends Component {
+export default class ShowCourseAdmin extends Component {
   constructor(props) {
     super(props);
-    // initialize the state with an empty todos array
     this.state = { todos: [], search: "" };
   }
 
-  //for searching event in page
   updateSearch(event) {
     this.setState({ search: event.target.value.substr(0, 20) });
   }
 
-  // To retrieve the todos data from the database --> use the componentDidMount lifecycle method
   componentDidMount() {
-    //to get data from mongo link
     axios
-      .get("http://localhost:5001/api/courses/")
+      .get("http://localhost:5001/api/course/")
       .then(response => {
         this.setState({ todos: response.data });
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
   }
+
   delete(id) {
-    console.log(id);
     axios
-      .delete("http://localhost:5001/course?id=" + id)
+      .delete(`http://localhost:5001/api/course/${id}`)
       .then(result => {
-        // this.forceUpdate()
-        // this.props.history.push("/showcourses/")
         toast.success("Deleted successfully");
       })
       .catch(err => {
-        // then print response status
         toast.error("Course not deleted");
       });
-    setTimeout(
-      function() {
-        //Start the timer
-        window.location.reload(); //After 1 second, set render to true
-      }.bind(this),
-      1300
-    );
+
+    setTimeout(() => {
+      window.location.reload();
+    }, 1300);
   }
 
-  // todoList() {
-  //     return this.state.todos.map(function(currentTodo, i){
-  //         // console.log(currentTodo.first_name)
-  //         return <Todo todo={currentTodo} key={i} />;
-
-  //     })
-  // }
-
   render() {
-    const divStyle = {
-      display: "contents"
-    };
-    // var message='You selected '+this.state.todos._id
-    const Todo = props => (
-      <div style={divStyle}>
-        <tr>
-          <td>{props.todo.courseName}</td>
-          <td>{props.todo.courseDescription}</td>
-          <td>{props.todo.instructor.email}</td>
-          <td>{props.todo.category.categoryName}</td>
-          <td>
-            {/* <Link to={"users/edit/"+props.todo._id}>Edit</Link> */}
-            {/* <button className="button muted-button" class="btn btn-success"><Link to={"users/edit/"+props.todo._id}>Edit</Link></button> */}
-            <a
-              href={"/ShowCourseList/edit/" + props.todo._id}
-              class="btn btn-primary btn-info"
-              role="button"
-              aria-pressed="true"
-            >
-              Edit
-            </a>
-            {/* <button onClick={this.delete.bind(this, props.todo._id)} class="btn btn-danger">Delete</button> */}
-            {/* <p>{message}</p> */}
-          </td>
-        </tr>
-      </div>
-    );
-    //used in filtering the content coming from database mongo
-    let filteredusers = this.state.todos.filter(course => {
+    const filteredCourses = this.state.todos.filter(course => {
       return (
-        course.courseName.indexOf(this.state.search) !== -1 ||
-        course.courseDescription.indexOf(this.state.search) !== -1 ||
-        course.category.categoryName.indexOf(this.state.search) !== -1 ||
-        course.instructor.email.indexOf(this.state.search) !== -1
+        course.courseName.toLowerCase().includes(this.state.search.toLowerCase()) ||
+        course.courseDescription.toLowerCase().includes(this.state.search.toLowerCase()) ||
+        course.category?.categoryName.toLowerCase().includes(this.state.search.toLowerCase()) ||
+        course.instructor?.email.toLowerCase().includes(this.state.search.toLowerCase())
       );
     });
+
     return (
       <div>
         <NavBar />
@@ -121,7 +74,7 @@ export default class UserList extends Component {
           <input
             type="text"
             placeholder="Search..."
-            class="form-control input-sm"
+            className="form-control input-sm"
             style={{ width: "250px" }}
             value={this.state.search}
             onChange={this.updateSearch.bind(this)}
@@ -131,11 +84,8 @@ export default class UserList extends Component {
         <div className="container" style={{ border: "10px solid lightgray" }}>
           <table
             className="table table-striped"
-            id="usertable"
+            id="coursetable"
             style={{ marginTop: 20 }}
-            ref={el => (this.el = el)}
-            data-order='[[ 1, "asc" ]]'
-            data-page-length="25"
           >
             <thead>
               <tr>
@@ -143,16 +93,35 @@ export default class UserList extends Component {
                 <th>Course Description</th>
                 <th>Instructor</th>
                 <th>Category</th>
-
                 <th>Action</th>
               </tr>
             </thead>
             <ToastContainer />
             <tbody>
-              {/* displaying data coming  */}
-              {filteredusers.map(function(currentTodo, i) {
-                return <Todo todo={currentTodo} key={i} />;
-              })}
+              {filteredCourses.map((course, i) => (
+                <tr key={i}>
+                  <td>{course.courseName}</td>
+                  <td>{course.courseDescription}</td>
+                  <td>{course.instructor?.email}</td>
+                  <td>{course.category?.categoryName}</td>
+                  <td>
+                    <a
+                      href={`/ShowCourseList/edit/${course._id}`}
+                      className="btn btn-primary btn-info"
+                      role="button"
+                    >
+                      Edit
+                    </a>{" "}
+                    &nbsp;
+                    <button
+                      onClick={() => this.delete(course._id)}
+                      className="btn btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
